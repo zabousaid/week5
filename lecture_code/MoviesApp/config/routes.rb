44ced546e287efe24b7e6e777afbbf1be481005b
Jeff Cohen <jeff@starterleague.com>
2013-05-02 12:28:04 -0500

    Code update

--- lecture_code/MoviesApp/config/routes.rb
+++ lecture_code/MoviesApp/config/routes.rb
@@ -1,9 +1,7 @@
 MoviesApp::Application.routes.draw do
 
  get "/movies", controller: 'movies', action: 'index'
- get "/movies/new", controller: 'movies', action: 'new'
 
  get '/movies/:id', controller: 'movies', action: 'show'
 
- post "/movies", controller: 'movies', action: 'create'
 end
