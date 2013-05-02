eb1cec0bae687024633f1b08731028a9fd27c178
Jeff Cohen <jeff@starterleague.com>
2013-05-02 16:55:40 -0500

    Finish all 7 routes and actions

--- lecture_code/MoviesApp/app/controllers/movies_controller.rb
+++ lecture_code/MoviesApp/app/controllers/movies_controller.rb
@@ -1,5 +1,23 @@
 class MoviesController < ApplicationController
 
+  def destroy
+    m = Movie.find_by_id(params["id"])
+    m.destroy
+    redirect_to "/movies"
+  end
+
+  def edit
+    @movie = Movie.find_by_id(params["id"])
+  end
+
+  def update
+    @movie = Movie.find_by_id(params["id"])
+    @movie.title = params[:title]
+    @movie.year = params[:year]
+    @movie.save
+    redirect_to "/movies"
+  end
+
   def create
     @movie = Movie.new
     @movie.title = params[:title]
@@ -13,8 +31,7 @@ class MoviesController < ApplicationController
   end
 
   def show
-    movie_id = params["id"]
-    @movie = Movie.find_by_id(movie_id)
+    @movie = Movie.find_by_id(params["id"])
   end
 
   def new
--- lecture_code/MoviesApp/config/routes.rb
+++ lecture_code/MoviesApp/config/routes.rb
@@ -3,9 +3,15 @@ MoviesApp::Application.routes.draw do
 # http://localhost:3000/movies/new
 
  get "/movies", controller: 'movies', action: 'index'
+ post "/movies", controller: 'movies', action: 'create'
+
 
  get '/movies/new', controller: 'movies', action: 'new'
  get '/movies/:id', controller: 'movies', action: 'show'
+ put '/movies/:id', controller: 'movies', action: 'update'
 
+ delete '/movies/:id', controller: 'movies', action: 'destroy'
+
+ get "/movies/:id/edit", controller: 'movies', action: 'edit'
 
 end
